6764fd1540c850cd6072b562b563193d7ef18bf9
AnuragSharma <anurag1609.m@gmail.com>
2026-01-13 12:41:41 +0530

    Controller updated

--- src/main/java/com/example/demo/ctr/ProductController.java
+++ src/main/java/com/example/demo/ctr/ProductController.java
@@ -3,6 +3,7 @@ package com.example.demo.ctr;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.Product;
 import com.example.demo.service.ProductService;
-
+@CrossOrigin(origins = "*")
 @RestController
 public class ProductController {
 	
@@ -42,6 +43,7 @@ public class ProductController {
 	}
 	
 //========================================================================================	
+	@CrossOrigin(origins = "*")
 	@GetMapping(value = "product/all")
 	public List<Product>fetchall() 
 	{
